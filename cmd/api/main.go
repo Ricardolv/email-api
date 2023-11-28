90d6e85f2cdc50626e0dadbf863009fb5a09883a
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2023-11-28 07:54:00 -0300

    DEVELOP add POST

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"email-api/internal/contract"
+	"email-api/internal/domain/campaign"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("SAPO!"))
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	service := campaign.Service{}
+
+	r.Post("/campaigns", func(w http.ResponseWriter, req *http.Request) {
+		var request contract.NewCampaign
+
+		err := render.DecodeJSON(req.Body, &request)
+		if err != nil {
+			println(err)
+		}
+
+		id, err := service.Create(request)
+		if err != nil {
+			render.Status(req, 400)
+			render.JSON(w, req, map[string]string{"error": err.Error()})
+			return
+		}
+
+		render.Status(req, 201)
+		render.JSON(w, req, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":3000", r)
